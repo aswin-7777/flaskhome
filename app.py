3b47203535db08dac26dd6b80bfbea88150189e0
aswin <aswinbhaskar07@gmail.com>
2026-09-16 14:29:46 +0530

    adding flask animation

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "welcome to the page"
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
